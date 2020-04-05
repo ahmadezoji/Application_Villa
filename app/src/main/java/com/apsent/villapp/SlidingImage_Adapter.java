a9fccaed6adb527650428ac0ccba120333c8e763
isee <isee.co.ir@gmail.com>
2020-04-05 18:37:24 +0430

    [CHANGE]resizable = false

--- app/src/main/java/com/apsent/villapp/SlidingImage_Adapter.java
+++ app/src/main/java/com/apsent/villapp/SlidingImage_Adapter.java
@@ -54,31 +54,6 @@ public class SlidingImage_Adapter extends PagerAdapter {
         if (imageModelArrayList.get(position)!=null)
             Glide.with(context).load(imageModelArrayList.get(position)).into(imageView);
 
-//        DelBtn = (Button)imageLayout.findViewById(R.id.slider_btnDel);
-//        imageView.setImageBitmap(imageModelArrayList.get(position));
-//        imageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                DelBtn.setVisibility(View.INVISIBLE);
-//            }
-//        });
-//        imageView.setOnLongClickListener(new View.OnLongClickListener() {
-//            @Override
-//            public boolean onLongClick(View v) {
-//                if(DelBtn.getVisibility() == View.INVISIBLE)
-//                    DelBtn.setVisibility(View.VISIBLE);
-//                else
-//                    DelBtn.setVisibility(View.INVISIBLE);
-//                return false;
-//            }
-//        });
-//        DelBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                imageView.setImageDrawable(context.getDrawable(R.drawable.add_selector));
-//                DelBtn.setVisibility(View.INVISIBLE);
-//            }
-//        });
         view.addView(imageLayout, 0);
 
         return imageLayout;
